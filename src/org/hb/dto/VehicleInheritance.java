7bc26adce799d975e0cf0bf1ef928627b097c8b5
Ahmed-Nasr-Ghazi <a7med.nasr99@gmail.com>
2022-01-21 14:55:34 +0200

    DiscriminatorColumn added

--- src/org/hb/dto/VehicleInheritance.java
+++ src/org/hb/dto/VehicleInheritance.java
@@ -1,9 +1,13 @@
 package org.hb.dto;
 
 import javax.persistence.CascadeType;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
@@ -11,6 +15,10 @@ import javax.persistence.Table;
 
 @Entity
 @Table (name = "Vehicle_Inheritance")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(
+		name = "Vehicle_Type",
+		discriminatorType = DiscriminatorType.STRING)
 public class VehicleInheritance {
 
 	@Id @GeneratedValue
