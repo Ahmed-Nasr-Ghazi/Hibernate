016b66564d4e7eda32984f0239d33c7528539c45
Ahmed-Nasr-Ghazi <a7med.nasr99@gmail.com>
2022-01-29 22:09:19 +0200

    understainding hibernate first level of caching

---
+++ src/org/hb/HibernateCashing.java
@@ -0,0 +1,45 @@
+package org.hb;
+
+import org.hb.dto.UserDetailsSimple;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateCashing {
+
+	public static void main(String[] args) {
+		
+		// create session factory
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(UserDetailsSimple.class)
+								.buildSessionFactory();
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		
+		try {
+			session.beginTransaction();
+
+			UserDetailsSimple userDetailsSimple = session.get(UserDetailsSimple.class, 1);
+			
+
+			
+			System.out.println(userDetailsSimple );
+			
+			session.getTransaction().commit();
+			
+			session = factory.getCurrentSession();
+			session.beginTransaction();
+
+			UserDetailsSimple userDetailsSimple2 = session.get(UserDetailsSimple.class, 1);
+			System.out.println(userDetailsSimple );
+
+			
+		}finally {
+			session.close();
+		}
+
+	}
+
+}
